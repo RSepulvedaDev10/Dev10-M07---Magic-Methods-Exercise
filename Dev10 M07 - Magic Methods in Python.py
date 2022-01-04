7c9c5c13a07e48e6d1bef0e804f9743a45cee4cd
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-04 13:59:42 -0600

    Update Dev10 M07 - Magic Methods in Python.py

--- Dev10 M07 - Magic Methods in Python.py
+++ Dev10 M07 - Magic Methods in Python.py
@@ -1,6 +1,35 @@
 import csv
        
-
+class Astronaut:
+    def __init__(self, name, flightHours, flightUnits, status):
+        self.name = name
+        self.flightHours = flightHours
+        self.flightUnits = flightUnits
+        self.status = status
+        
+    def __str__(self, name, status):
+        return self.name, self.status
+        
+    def __gt__(self, other):
+        print('__gt__ called - self: %s, other: %s' % (self,other))
+        if self.flightUnits == other.flightUnits:
+            return self.flightHours > other.flightHours
+        else:
+            return False
+    
+    def __eq__(self, other):
+        ('__eq__ called')
+        if self.flightUnits == other.flightUnits:
+            return self.flightHours == other.flightHours
+        else:
+            return False
+        
+    def __ge__(self, other):
+        ('__ge__ called')
+        if self.flightUnits == other.flightUnits:
+            return self.flightHours >= other.flightHours
+        else:
+            return False
         
 findings = []
 with open("astronauts.csv","r") as csvfile:
@@ -8,7 +37,5 @@ with open("astronauts.csv","r") as csvfile:
     for row in astronautReader:
         finding = (row['Name'],row['Year'],row['Group'],row['Status'],row['Birth Date'],row['Birth Place'],row['Gender'],row['Alma Mater'],row['Undergraduate Major'],row['Graduate Major'],row['Military Rank'], row['Military Branch']),row['Space Flights'],row['Space Flight (hr)'],row['Space Walks'],row['Space Walks (hr)'],row['Missions'],row['Death Date'], row['Death Mission']
         findings.append(finding)
-
-print(findings[0])
-
-print(findings[0] > findings[1])
+        
+print findings[0]
